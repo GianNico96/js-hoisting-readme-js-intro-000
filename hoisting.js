9c3ad84a23ffd5dc957495b759bc9908dc4e3b77
looping-torrent-8105 <gianherrera96@gmail.com>
2020-09-01 04:37:39 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -18,11 +18,11 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  sayMy();
   var name = "Cricky";
-
+  
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+    sayMy();
 }
